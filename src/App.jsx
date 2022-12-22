6aa26b255bb55519c67ebcfa0b2628d9de2b8e29
HardtoPronounce <60298101+HardtoPronounce@users.noreply.github.com>
2022-12-22 19:32:58 +0200

    mesaj

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Patrat from './components/Patrat'
 import './App.css';
 
 function App() {
+  let dim = [100, 250, 300, 200];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {dim.map(e=>(<Patrat width={String(e)+"vw"}/>))}
     </div>
   );
 }
---
+++ src/components/Patrat.jsx
@@ -0,0 +1,9 @@
+import "./componentStyles/style1.css"
+
+function Patrat(props) {
+    return (
+      <div className="Patrat" style={{width:props.width}}></div>
+    )
+  }
+
+export default Patrat;
